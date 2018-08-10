156a2f87de264f8b8270ec4bd382006a100ec87b
gustavo <g.92oliveira@gmail.com>
2018-08-10 14:15:05 -0300

    configuracao do swagger

--- cerveja-ciclic/src/main/java/com/gustavo/cervejaciclic/CervejaCiclicApplication.java
+++ cerveja-ciclic/src/main/java/com/gustavo/cervejaciclic/CervejaCiclicApplication.java
@@ -1,12 +1,39 @@
 package com.gustavo.cervejaciclic;
 
+import com.gustavo.cervejaciclic.model.Cerveja;
+import com.gustavo.cervejaciclic.repository.CervejaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class CervejaCiclicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CervejaCiclicApplication.class, args);
+	}
+}
+
+
+@Component
+class CreateDb implements CommandLineRunner {
+
+	@Autowired
+	private CervejaRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Cerveja("Weissbier", -1, 3));
+		repository.save(new Cerveja("Pilsens"	,-2 , 4));
+		repository.save(new Cerveja("Weizenbier",	-4, 6));
+		repository.save(new Cerveja("Red ale"	,-5, 5));
+		repository.save(new Cerveja("India pale ale",	-6, 7));
+		repository.save(new Cerveja("IPA"	,-7, 10));
+		repository.save(new Cerveja("Dunkel",	-8 , 2));
+		repository.save(new Cerveja("Imperial Stouts",	-10, 13));
+		repository.save(new Cerveja("Brown ale",0,14));
+
 	}
 }
