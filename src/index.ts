2935e703dcb6b88f768215087f31197096997c55
Gustavo <11997211+guhenrique007@users.noreply.github.com>
2023-04-13 23:54:06 -0300

    Add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
+const { PORT = 3000 } = process.env; 
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor iniciado na porta ${PORT}`);
 });
