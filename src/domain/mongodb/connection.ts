03835828ad56c3cebc0b2f96e392c3a8812de424
Gustavo <11997211+guhenrique007@users.noreply.github.com>
2023-04-14 18:24:07 -0300

    Update docker configs

---
+++ src/domain/mongodb/connection.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
+
+export const connect = () => {
+  const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+
+  console.log("Trying to connect to database...");
+  mongoose
+    .connect(uri)
+    .catch(error => console.log(error));
+  mongoose.connection.on("error", () => console.log('connection error:'));
+  mongoose.connection.once("open", () => console.log('database connected'));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
 import dotenv from 'dotenv';
 import { expressAdapter } from './ports_and_adapters/server/expressAdapter';
 import { castStringToNumber } from './helpers/helpers';
+import { connect } from './domain/mongodb/connection';
 
 dotenv.config();
 const PORT: number = castStringToNumber(process.env.PORT, 3000)
 
-const server = expressAdapter()
-console.log(PORT)
-server.start(PORT);
+const server = expressAdapter();
+console.log(PORT);
+
+(() => {
+    server.start(PORT);
+    connect()
+}) ();
+
