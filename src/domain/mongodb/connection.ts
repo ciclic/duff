bfb741ae18a8f54eec5983c341965437c6590c33
Gustavo <11997211+guhenrique007@users.noreply.github.com>
2023-04-14 22:48:38 -0300

    Add port adapater to database

--- src/domain/mongodb/connection.ts
+++ src/domain/mongodb/connection.ts
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import { DatabasePort } from "../../ports_and_adapters/database/databasePort";
 dotenv.config();
 
 const { DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
-export const connect = () => {
-  const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+export const MongoAdapter: DatabasePort = () => {
+  const connect = () => {
+    const uri = `mongodb://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
-  console.log("Trying to connect to database...");
-  mongoose
-    .connect(uri)
-    .catch(error => console.log(error));
-  mongoose.connection.on("error", () => console.log('connection error:'));
-  mongoose.connection.once("open", () => console.log('database connected'));
+    console.log("Trying to connect to database...");
+    mongoose
+      .connect(uri)
+      .catch(error => console.log(error));
+    mongoose.connection.on("error", () => console.log('connection error:'));
+    mongoose.connection.once("open", () => console.log('database connected'));
+  }
+
+  return {
+    connect
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
 import dotenv from 'dotenv';
 import { expressAdapter } from './ports_and_adapters/server/expressAdapter';
 import { castStringToNumber } from './helpers/helpers';
-import { connect } from './domain/mongodb/connection';
+import { MongoAdapter } from './domain/mongodb/connection';
 
 dotenv.config();
 const PORT: number = castStringToNumber(process.env.PORT, 3000)
 
 const server = expressAdapter();
-console.log(PORT);
+
+const database = MongoAdapter();
 
 (() => {
     server.start(PORT);
-    connect()
+    database.connect()
 }) ();
 
