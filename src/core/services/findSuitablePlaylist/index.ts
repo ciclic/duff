3f8e1b3d03840ae50b56388b4fd2a26f23d541e2
Gustavo <11997211+guhenrique007@users.noreply.github.com>
2023-04-16 16:22:08 -0300

    Removing unecessary imports

--- src/core/services/findSuitablePlaylist/index.ts
+++ src/core/services/findSuitablePlaylist/index.ts
@@ -1,11 +1,7 @@
 import dotenv from 'dotenv';
-import SpotifyWebApi from 'spotify-web-api-node';
 import { getAuth } from '../spotifyAuth';
 import { spotifyApi } from '../../../infra/http/integrations/spotify';
 dotenv.config();
-
-const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
-
 
 export const findSuitablePlaylist = async (beerStyle: string) => {
   try {
