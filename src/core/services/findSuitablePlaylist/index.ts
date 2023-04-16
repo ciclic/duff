633fa3eddf430d03295e833f77ba273c96818fa9
Gustavo <11997211+guhenrique007@users.noreply.github.com>
2023-04-16 16:20:35 -0300

    Move spotify integrator

--- src/core/services/findSuitablePlaylist/index.ts
+++ src/core/services/findSuitablePlaylist/index.ts
@@ -1,18 +1,13 @@
 import dotenv from 'dotenv';
 import SpotifyWebApi from 'spotify-web-api-node';
 import { getAuth } from '../spotifyAuth';
+import { spotifyApi } from '../../../infra/http/integrations/spotify';
 dotenv.config();
 
 const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
 
 
 export const findSuitablePlaylist = async (beerStyle: string) => {
-  const spotifyApi = new SpotifyWebApi({
-      clientId: SPOTIFY_CLIENT_ID,
-      clientSecret: SPOTIFY_CLIENT_SECRET,
-      redirectUri: 'http://www.example.com/callback'
-    });
-
   try {
     const token = await getAuth();
     spotifyApi.setAccessToken(token);
